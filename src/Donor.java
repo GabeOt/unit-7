3a74c5e76bb7e12edcbe383ca798b11ac29fdacc
GabeOt <gabriel.otridge@ssfs.org>
2022-02-09 08:57:02 -0500

    worked on the exercises

--- src/Donor.java
+++ src/Donor.java
@@ -1,4 +1,17 @@
 public class Donor {
+    private double amount;
+    private String name;
+    public Donor(double amount, String name) {
+        this.amount = amount;
+        this.name = name;
+    }
 
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
--- src/DonorRunner.java
+++ src/DonorRunner.java
@@ -11,8 +11,8 @@ public class DonorRunner {
         donorList = new ArrayList<>();
         getDonorList("files/donors.txt");
         sortDonorList();
-        //System.out.println(donorList.get(0).getName()); // Should print out Hernandez
-        //System.out.println(donorList.get(donorList.size()-1).getName()); // Should print out Hall
+        System.out.println(donorList.get(0).getName()); // Should print out Hernandez
+        System.out.println(donorList.get(donorList.size()-1).getName()); // Should print out Hall
         System.out.println(getDonatedAmount("Anderson")); // should print 1436.5
         System.out.println(getDonatedAmount("Roberts")); // should print 307.86
         System.out.println(getDonatedAmount("Wilkinson")); // should print 0.0
@@ -20,10 +20,29 @@ public class DonorRunner {
     }
 
     public static void getDonorList(String filename) throws IOException {
+        Scanner scan = new Scanner(new File(filename));
+        while (scan.hasNext()) {
+            String donor = scan.next();
+            double amount = scan.nextDouble();
+            Donor d = new Donor(amount, donor);
+            donorList.add(d);
+
+        }
 
     }
 
     public static void sortDonorList() {
+        for (int i=0; i <donorList.size(); i++) {
+            int biggest =i;
+            for (int j=i+1; j<donorList.size(); j++) {
+                if (donorList.get(i).getAmount() > donorList.get(biggest).getAmount())
+                    biggest = j;
+
+            }
+            Donor temp = donorList.get(i);
+            donorList.set(i, donorList.get(biggest));
+            donorList.set(biggest, temp);
+        }
 
     }
 
