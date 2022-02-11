9b6319785d577a209b43a9c1636d52b76f5e423a
GabeOt <gabriel.otridge@ssfs.org>
2022-02-11 10:21:24 -0500

    worked on the exercises

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -5,6 +5,44 @@ import java.util.Scanner;
 
 public class ArrayListAlgorithms {
 
+    public static ArrayList<Integer> fileDuplicates(String file1, String file2) throws FileNotFoundException {
+            Scanner input1 = new Scanner(new File(file1));
+            ArrayList<Integer> list1 = new ArrayList<>();
+            while (input1.hasNext()) {
+                list1.add(input1.nextInt());
+            }
+
+            Scanner input2 = new Scanner(new File(file2));
+            ArrayList<Integer> list2 = new ArrayList<>();
+            while (input2.hasNext()) {
+                list2.add(input2.nextInt());
+            }
+
+        ArrayList list = new ArrayList<>();
+
+            for (int i=0; i<list1.size(); i++) {
+                int x = list1.get(i);
+                if (list2.contains(x)) {
+                    list.add(x);
+                }
+
+            }
+            ArrayList list3 = new ArrayList<>();
+            for (int i=0; i<list1.size(); i++) {
+                int x =list1.get(i);
+                if (list2.contains(x)) {
+                    if (!list3.contains(x)) {
+                        list3.add(x);
+                    }
+                }
+            }
+            return list3;
+
+
+
+    }
+
+
     /*
     public static ArrayList<Integer> fileDuplicates(String file1, String file2) throws FileNotFoundException {
 
